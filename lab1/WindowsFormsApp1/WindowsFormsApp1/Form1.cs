3d3984057a878d91c215fa72514f2da82402f71a
BorisZhur <50907618+BorisZhur@users.noreply.github.com>
2020-04-05 00:35:34 +0300

    1

--- lab1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ lab1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,11 +16,30 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        public class cdr
+        {
+            DateTime Timestamp;
+            string Origin;
+            string Destination;
+            float MinCount;
+            int SmsCount;
 
+        }
+        public float tarificate(float callDuration, int smsNumber, int MinP1, int SmsP1, int minPriceP1, int smsPriceP1, int minPriceP2, int smsPriceP2)
+        {
+            float price = 0;
+            if (callDuration < MinP1) { price = callDuration * minPriceP1; }
+            else { price = MinP1 * minPriceP1 + (callDuration - MinP1) * minPriceP2; }
+            if (smsNumber < SmsP1) { price += smsNumber * smsPriceP1; }
+            else { price += SmsP1 * smsPriceP1 + (smsNumber - SmsP1) * smsPriceP2; }
+            return price;
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
         }
+        
+        
     }
 }
